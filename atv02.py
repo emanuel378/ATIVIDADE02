b0073329bf00b14107d4e269807be2bcc214fac2
emanuel378 <emanuelmvieira10@gmail.com>
2024-09-07 17:57:59 -0300

    Celsius para Fahrenheit

---
+++ atv02.py
@@ -0,0 +1,5 @@
+#Crie um programa que converta uma temperatura dada em Celsius para Fahrenheit usando a fórmula: F = C * 9/5 + 32.
+
+cel=float(input('Digite a temperatura:'))
+formu=cel* 9/5 + 32
+print(f"A sua temperatura de em Celsius ficou {formu} em fahrenheit")
